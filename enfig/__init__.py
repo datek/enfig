8a0b3add36f11a01127cfd5159768a5714f290e8
Attila Dudas <dudasa7@gmail.com>
2025-07-24 08:48:49 +0200

    Initial commit

---
+++ enfig/__init__.py
@@ -0,0 +1,9 @@
+from enfig.base import BaseConfig
+from enfig.errors import ValidationError, ConfigAttributeError
+
+__all__ = [
+    BaseConfig.__name__,
+    ValidationError.__name__,
+    ConfigAttributeError.__name__,
+]
+
